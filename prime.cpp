e193102f4772f954c241fe39a25f6d31587878ec
Artificialvira <arivarasana.aids2022@citchennai.net>
2024-11-23 06:47:54 +0530

    added dsa problems for 22/11

--- prime.cpp
+++ prime.cpp
@@ -32,7 +32,32 @@ using namespace std;
 
 // }
 
-set<int> seiveoferatosthenes(int n, int start)
+// set<int> seiveoferatosthenes(int n, int start)
+// {
+//     vector<bool> hash(n+1,true);
+//     hash[0] = false;
+//     hash[1] = false;
+
+//     for (int i = 2; i <= n; i++)
+//     {
+//         if(hash[i])
+//         {
+//             for (int p = i * i; p <= n; p += i)
+//             {
+//                 hash[p] = false;
+//             }
+//         }
+//     }
+//     set<int> result;
+//     for (int i = start; i <= n; i++)
+//     {
+//         if(hash[i]) result.insert(i);
+//     }
+
+//     return result;
+// }
+
+vector<int> seiveoferatosthenes(int n)
 {
     vector<bool> hash(n+1,true);
     hash[0] = false;
@@ -48,10 +73,10 @@ set<int> seiveoferatosthenes(int n, int start)
             }
         }
     }
-    set<int> result;
-    for (int i = start; i <= n; i++)
+    vector<int> result;
+    for (int i = 0; i <= n; i++)
     {
-        if(hash[i]) result.insert(i);
+        if(hash[i]) result.push_back(i);
     }
 
     return result;
@@ -59,11 +84,28 @@ set<int> seiveoferatosthenes(int n, int start)
 
 int main()
 {
-    int start, end;
-    cout<<"enter the start range: ";
-    cin>> start;
-    cout<<"enter the end range: ";
-    cin>>end;
+    // int start, end;
+    // cout<<"enter the start range: ";
+    // cin>> start;
+    // cout<<"enter the end range: ";
+    // cin>>end;
 
-    for (int i : seiveoferatosthenes(end,start)) cout<<i<<" ";
+    int target;
+    cin>>target;
+
+    vector<int> primes = seiveoferatosthenes(target);
+    int i = 0, j = primes.size()-1;
+
+    while(i < j)
+    {
+        int num = primes[i] + primes[j];
+        cout<<num<<" ";
+        if(num == target) 
+        {
+            cout<<"yes"<<" ";
+            break;
+        }
+        else if(target > num) i++;
+        else j--;
+    }
 }
